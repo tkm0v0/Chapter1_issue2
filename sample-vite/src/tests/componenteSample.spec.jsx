3cf1a1c8a676990db9d8a9fc31657120df0f68fc
tkm0v0 <maig15480@gmail.com>
2024-10-16 13:48:51 +0900

    解決策：エクスポートとインポートを一致させる

--- sample-vite/src/tests/componenteSample.spec.jsx
+++ sample-vite/src/tests/componenteSample.spec.jsx
@@ -1,4 +1,4 @@
-import App from "../Todo.jsx";
+import { Todo } from "../Todo.jsx"; // 名前付きインポート
 import React from "react";
 import '@testing-library/jest-dom'
 import { render, screen } from "@testing-library/react";
@@ -6,7 +6,7 @@ import { render, screen } from "@testing-library/react";
 describe("Title Test", () => {
   it("タイトルがHello Jestであること", () => {
     // testId(title)を指定して取得
-    render(<App />);
+    render(<Todo />);
     const title = screen.getByTestId("title");
     expect(title).toHaveTextContent("Hello Jest");
   });
