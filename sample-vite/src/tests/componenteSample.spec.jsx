f62883a4ed0b8cd9a405c21de0959561ea79d095
tkm0v0 <maig15480@gmail.com>
2024-10-16 13:54:59 +0900

    テスト内で非同期処理を待機する

--- sample-vite/src/tests/componenteSample.spec.jsx
+++ sample-vite/src/tests/componenteSample.spec.jsx
@@ -1,13 +1,13 @@
-import { Todo } from "../Todo.jsx"; // 名前付きインポート
+import { Todo } from "../Todo.jsx";
 import React from "react";
-import '@testing-library/jest-dom'
+import '@testing-library/jest-dom';
 import { render, screen } from "@testing-library/react";
 
 describe("Title Test", () => {
-  it("タイトルがHello Jestであること", () => {
-    // testId(title)を指定して取得
+  it("タイトルがHello Jestであること", async () => { // テスト関数をasyncに
     render(<Todo />);
-    const title = screen.getByTestId("title");
+    // `findByTestId`は要素が見つかるまで待機します
+    const title = await screen.findByTestId("title");
     expect(title).toHaveTextContent("Hello Jest");
   });
 });
